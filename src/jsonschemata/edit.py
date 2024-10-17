a9d86ae1af00244df3e036440c315ecd4502515a
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-10-17 12:32:05 +0200

    Release version 0.0.0.dev23

--- src/jsonschemata/edit.py
+++ src/jsonschemata/edit.py
@@ -18,10 +18,39 @@ def required_last(schema: dict) -> dict:
         if key in schema:
             for subschema in schema[key]:
                 required_last(subschema)
-    for key in ["if", "then", "else", "not", "items", "additionalProperties"]:
+    for key in ["if", "then", "else", "not", "items", "unevaluatedItems", "contains", "additionalProperties", "patternProperties", "unevaluatedProperties"]:
         if key in schema and isinstance(schema[key], dict):
             required_last(schema[key])
     if "properties" in schema and isinstance(schema["properties"], dict):
         for subschema in schema["properties"].values():
             required_last(subschema)
+    return schema
+
+
+def add_property(schema: dict, prop: str, value: dict) -> dict:
+    """Recursively add a property to a JSON schema.
+
+    Parameters
+    ----------
+    schema : dict
+        The JSON schema to modify.
+    prop : str
+        The name of the property to add.
+    value : Any
+        The value of the property to add.
+
+    Returns
+    -------
+    dict
+        The modified schema.
+    """
+    if "properties" in schema:
+        schema["properties"][prop] = value
+    for key in ["anyOf", "allOf", "oneOf", "prefixItems"]:
+        if key in schema:
+            for subschema in schema[key]:
+                add_property(subschema, prop, value)
+    for key in ["if", "then", "else", "not", "items", "unevaluatedItems", "contains", "additionalProperties", "patternProperties", "unevaluatedProperties"]:
+        if key in schema and isinstance(schema[key], dict):
+            add_property(schema[key], prop, value)
     return schema
