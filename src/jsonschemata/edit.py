6d5d639940fab5b5974a12ff9d414440cece0b83
AAriam <80158628+AAriam@users.noreply.github.com>
2024-11-01 21:08:35 +0100

    Release version 0.0.0.dev29

--- src/jsonschemata/edit.py
+++ src/jsonschemata/edit.py
@@ -1,3 +1,11 @@
+from __future__ import annotations as _annotations
+
+from typing import TYPE_CHECKING as _TYPE_CHECKING
+
+if _TYPE_CHECKING:
+    from typing import Callable
+
+
 def required_last(schema: dict) -> dict:
     """Modify JSON schema to recursively put all 'required' fields at the end of the schema.
 
@@ -27,7 +35,13 @@ def required_last(schema: dict) -> dict:
     return schema
 
 
-def add_property(schema: dict, prop: str, value: dict) -> dict:
+def add_property(
+    schema: dict,
+    prop: str,
+    value: dict,
+    conditioner: Callable[[dict, list], bool] | None = None,
+    current_path: list[str] | None = None,
+) -> dict:
     """Recursively add a property to a JSON schema.
 
     Parameters
@@ -38,21 +52,28 @@ def add_property(schema: dict, prop: str, value: dict) -> dict:
         The name of the property to add.
     value : Any
         The value of the property to add.
-
+    conditioner : Callable[[dict, list], bool], optional
+        A function that takes a schema and the path to the current schema as arguments
+        and returns a boolean indicating whether to add the property to the schema.
+    current_path : list[str], optional
+        The path to the current schema in the JSON schema hierarchy.
     Returns
     -------
     dict
         The modified schema.
     """
-    if "properties" in schema:
-        schema["properties"][prop] = value
-        for subschema in schema["properties"].values():
-            add_property(subschema, prop, value)
+    if current_path is None:
+        current_path = []
     for key in ["anyOf", "allOf", "oneOf", "prefixItems"]:
         if key in schema:
-            for subschema in schema[key]:
-                add_property(subschema, prop, value)
+            for idx, subschema in enumerate(schema[key]):
+                add_property(subschema, prop, value, conditioner, current_path + [f"{key}[{idx}]"])
     for key in ["if", "then", "else", "not", "items", "unevaluatedItems", "contains", "additionalProperties", "patternProperties", "unevaluatedProperties"]:
         if key in schema and isinstance(schema[key], dict):
-            add_property(schema[key], prop, value)
+            add_property(schema[key], prop, value, conditioner, current_path + [key])
+    if "properties" in schema:
+        for property_name, subschema in schema["properties"].items():
+            add_property(subschema, prop, value, conditioner, current_path + [f"properties[{property_name}]"])
+        if not conditioner or conditioner(schema, current_path):
+            schema["properties"][prop] = value
     return schema
